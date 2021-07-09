356ee61af16d53672ca3b98f91112b84b176b974
456sakash <87191018+456sakash@users.noreply.github.com>
2021-07-09 19:27:43 +0530

    mul

--- sunny.java
+++ sunny.java
@@ -3,4 +3,14 @@ public class akash{
     int a=9;
     int v=89;
     int g=a+v;
-    System.out.println(" the addition two numbers",g);
+    System.out.println(" the addition two numbers",+g);
+    int o=a-v;
+    System.out.println(I"the subtraction of two numbers:",+o);
+    int f=a*v;
+    System.out.println("the multiplication of two numbers:",+f);
+    
+    
+    
+    
+    
+    
